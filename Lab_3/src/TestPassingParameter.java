e52f18fd9ec4966bf276eab2538b6e43f83f724a
wfiftyfour <buiviethung261104@gmail.com>
2024-11-26 21:48:01 +0700

    add

---
+++ Lab_3/src/TestPassingParameter.java
@@ -0,0 +1,25 @@
+
+public class TestPassingParameter {
+	public static void main (String[] args) {
+		DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+		DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+		
+		swap(jungleDVD, cinderellaDVD);
+		System.out.println("Jungle DVD's title: " + jungleDVD.getTitle());
+		System.out.println("Cinderella DVD's title: " + cinderellaDVD.getTitle());
+		
+		changeTitle(jungleDVD, cinderellaDVD.getTitle());
+		System.out.println("Jungle DVD's title: " + jungleDVD.getTitle());
+	}
+    public static void swap(DigitalVideoDisc o1, DigitalVideoDisc o2){
+        String tmp = o1.getTitle();
+        o1.setTitle(o2.getTitle());
+        o2.setTitle(tmp);
+    }
+
+	public static void changeTitle(DigitalVideoDisc dvd, String title){
+		String oldTitle = dvd.getTitle();
+		dvd.setTitle(title);
+		dvd = new DigitalVideoDisc(oldTitle);
+	}
+}
